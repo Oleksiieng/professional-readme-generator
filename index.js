fec7d5b471734815a1d47a0e5e6847d7511837da
Oleksii Kulykov <oleksii.eng@gmail.com>
2024-02-06 21:46:13 +0000

    added questions

--- index.js
+++ index.js
@@ -5,7 +5,42 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Project title:',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Project description:',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Installation instructions:',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Usage information:',
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Contribution guidelines:',
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Test instructions:',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Choose a license for your project:',
+        choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3-Clause', 'None'],
+    }
 ];
 
 // function to write README file
